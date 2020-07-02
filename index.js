c0ba00c14bb118bbd62587b799a3f30ae45814ef
Alejandro Morales <alejo89.morales89@gmail.com>
2020-07-02 14:02:33 -0500

    Primer subida proyecto

---
+++ index.js
@@ -0,0 +1,3 @@
+$(function(){//se va ejecutar cuando se termine de cargar el sitio y demás scripts, recién hay se va ejecutar esa función
+  $('[data-toggle="tooltip"]').tooltip()//agarra todos los elementos data-toogle="tooltip" y llamamos la funcion .tooltip() de boostrapt que viene incluida en jquery
+});
